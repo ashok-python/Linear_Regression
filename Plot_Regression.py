eb9a08af912b40ab39d72b28118f13338c852335
Ashok Kumar T <60009759+ashok-python@users.noreply.github.com>
2020-04-20 14:46:14 +0530

    Plot scatter and Linear Regression Line
    1.Plot scatter plot from x and y lists with random numbers
    2.Plot Linear Regression Line  and see in any iteration the line hits any actual values. 
    3.If not, keep changing m and c in straight line and keep trying with new random numbers in x and y axis.

---
+++ Plot_Regression.py
@@ -0,0 +1,68 @@
+#Plot Linear Regression Line and find out Regressors count
+
+#Create Lists for X and Y coordinates with random numbers
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x1=[] #Initiate X List
+for i in range(10):
+    r=np.random.randint(10)
+    x1.append(r)
+
+print(x1)
+y1=[]       #Initiate Y list
+for i in range(10):
+    r=np.random.randint(20)
+    y1.append(r)
+print(y1)
+
+import pandas as pd
+plt.scatter(x1,y1)  #Plot Scatter Plotting using x1 and y1 lists
+#
+#   Now draw Line using y=mx+c connecting these points
+#   Also find out how points we can connect
+#   Keep tracking Regressors
+#
+any_matched = False
+for i in range(10):
+    #yhat = mx + c here c=1.5, m=i+1
+    yhat=list(map(lambda x: ((i+1)*x + 1.5),x1)) #Predicting values with Linear Regression line
+    yhat = [int(j) for j in yhat]   #converting to integers
+    print("\nActual=",y1)
+    print("\nPredicted=",yhat)
+    plt.plot(x1,yhat,lw=4)  #Plotting Straight line
+    #Find the diff between actual and predicted
+    y1s = pd.Series(y1) #Convert list to Series
+    yhats = pd.Series(yhat)
+    diff = y1s - yhats #Find diff actual vs predicted for Regressors counting
+    print("\n\t Iteration {} results".format(i+1))
+#
+#   Print min and max details
+#
+#
+#Compute actual vs predicted
+#
+    print("\nmin = ",diff.min())
+    print("\nmax = ",diff.max())
+    print("\nmean = ",diff.mean())
+    print("\nstd =",diff.std())
+#
+#Compute Residuals
+#
+    print("\nCount those matched=",list(diff).count(0)) #how many it could touch
+    print("\nResidual count = ",len(diff)-list(diff).count(0))
+    plt.pause(1)
+
+    if (list(diff).count(0)) > 0:
+            print("\nIteration {} could hit somepoints {}:".format(i,list(diff).count(0)))
+            any_matched = True
+            last_hit = i + 1
+plt.show()
+
+if(any_matched):
+    print("\n Check last iteration",last_hit)
+else:
+    print("\n No iteration could hit atleast one")
+
+#Keep changing m and c and find results
